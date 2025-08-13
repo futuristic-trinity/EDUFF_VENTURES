a12d580555be2e0359be8343f7ed10c7b18e132f
TRINITY <fredcomputing@gmail.com>
2025-08-13 11:03:45 +0000

    settings static files modified

--- edu_env/Eduff_Sports/Eduff_Sports/settings.py
+++ edu_env/Eduff_Sports/Eduff_Sports/settings.py
@@ -155,16 +155,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+    ]
 
 #serving static files during production
 #STATIC_ROOT = BASE_DIR / "staticfiles"
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # This production code might break development mode, so we check whether we're in DEBUG mode
 
     # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-STATIC_ROOT = BASE_DIR /'staticfiles'
+#STATIC_ROOT = BASE_DIR /'staticfiles'
 
     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
     # and renames the files with unique names for each version to support long-term caching
